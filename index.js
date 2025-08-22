9248839041a5d8491e720173ba150bf823c1fc85
DevWizardMarcos <email@devwizardmarcos.com>
2025-08-22 20:23:31 -0300

    Progresso no projeto, ainda incompleto

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from "express"
+import { readFileSync } from "fs"
 
 // const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  try{
+    const fileBuffer = readFileSync('./data.json')
+    const fileText = fileBuffer.toString()
+    const fileData = JSON.parse(fileText)
+
+    res.send(fileData)
+  }catch(error){
+    console.log("Algo de errado aconteceu...")
+    res.status(500).send("Internal server error")
+  }
+})
+
+app.post("/", (request, response) =>{
+    try{
+        console.log(request.body)
+        
+    }catch(error){
+        console.log("Algo deu errado!")
+    }
 })
 
 app.get("/teste", function(request, response){
