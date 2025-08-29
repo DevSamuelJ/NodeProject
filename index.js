3ea7362fb897f413cec696e9baf601a914db7a73
DevWizardMarcos <email@devwizardmarcos.com>
2025-08-29 18:46:50 -0300

    Ultimas etapas do projeto

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-import express from "express"
-import { readFileSync } from "fs"
+import express from 'express'
+import { readFileSync, writeFileSync } from 'fs'
 
 
 const app = express()
 app.use(express.json())
-const port = 5000
+const port = 3333
 
 app.get('/', (req, res) => {
-  try{
-    const fileBuffer = readFileSync('./data.json')
-    const fileText = fileBuffer.toString()
-    const fileData = JSON.parse(fileText)
+    try {
+        const fileBuffer = readFileSync('./data.json')
+        const fileText = fileBuffer.toString()
+        const fileData = JSON.parse(fileText)
 
-    res.send(fileData)
-  }catch(error){
-    console.log("Algo de errado aconteceu...")
-    res.status(500).send("Internal server error")
-  }
-})
-
-app.post("/", (request, response) =>{
-    try{
-        console.log(request.body)
-
-    }catch(error){
-        console.log("Algo deu errado!")
+        res.send(fileData)
+    } catch (error) {
+        console.log("Algo inesperado ocorreu =/")
+        res.status(500).send('Internal server error')
     }
 })
 
-app.get("/teste", function(request, response){
-    response.send("mensagem de teste")
+app.post('/', (request, response) => {
+    try {
+        const fileBuffer = readFileSync('./data.json')
+        const fileText = fileBuffer.toString()
+        const highScores = JSON.parse(fileText)
+
+        // console.log(request.body)
+        const playerIndex = highScores.findIndex(element => element.player === request.body.player)
+
+        if (playerIndex === -1) {
+            highScores.push({
+                player: request.body.player,
+                score: request.body.score
+            })
+        } else if (request.body.score < highScores[playerIndex].score) {
+            highScores[playerIndex].score = request.body.score
+        }
+
+        writeFileSync("./data.json", JSON.stringify(highScores))
+
+        response.send({message:'OK'})
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message:'Internal server error'})
+    }
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
